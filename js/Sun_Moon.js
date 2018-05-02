09daf59fe033ded6cf28ddc7eded73b8ca8fceee
KERRAD <KERRAD@intechS1.fr>
2018-05-02 10:21:42 +0200

    grpoekg

--- js/Sun_Moon.js
+++ js/Sun_Moon.js
@@ -30,7 +30,7 @@ var req_abandon_solo = require("./req_abandon_solo.js");
 var req_abandon_multi = require("./req_abandon_multi.js");
 var req_option = require("./req_option.js");
 var req_inscrire = require("./req_inscrire.js");
-
+var req_identifier = require("./req_identifier.js");
 var req_statique = require("./req_statique.js");
 var req_erreur = require("./req_erreur.js");
 
@@ -117,6 +117,9 @@ var traite_requete = function (req, res) {
 				break;
 			case '/req_inscrire':
 				req_inscrire(req, res, query);
+				break;
+			case '/req_identifier':
+				req_identifier(req, res, query);
 				break;
 			case '/req_erreur':
 				req_erreur(req, res, query);
