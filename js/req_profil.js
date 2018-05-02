2ded4f608628279423cf5d8b0ed0267b09e03037
ABIDI <ABIDI@intechS1.fr>
2018-05-02 11:41:47 +0200

    profil

---
+++ js/req_profil.js
@@ -0,0 +1,27 @@
+"ues strict";
+
+var fs = require("fs");
+require('remedial');
+
+var trait = function (req, res, query) {
+
+	var marqueurs;
+	var page;
+
+	// AFFICHAGE DE LA PAGE PROFIL
+
+	page = fs.readFileSync('../html/modele_profil.html', 'utf-8');
+
+	marqueurs = {};
+	marqueurs.erreur = "";
+	marqueurs.pseudo = "";
+	marqueurs.mdp = "";
+	page = page.supplant(marqueurs);
+
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write(page);
+	res.end();
+
+};
+
+module.exports = trait;
