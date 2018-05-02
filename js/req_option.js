1d19547b9eb6f5631fc679ee33a58b71d190f8b9
KERRAD <KERRAD@intechS1.fr>
2018-05-02 11:39:45 +0200

    oui

---
+++ js/req_option.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var fs = require("fs");
+require('remedial');
+
+var trait = function (req, res, query) {
+
+	var marqueurs;
+	var page;
+
+	// ALLER A LA PAGE OPTION
+
+	page = fs.readFileSync('../html/modele_options.html', 'utf-8');
+
+	marqueurs = {};
+	marqueurs.erreur = "";
+	page = page.supplant(marqueurs);
+
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write(page);
+	res.end();
+};
+
+module.exports = trait;
---
+++ js/req_succes.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var fs = require("fs");
+require('remedial');
+
+var trait = function (req, res, query) {
+
+	var marqueurs;
+	var pseudo;
+	var mdp;
+	var page;
+	
+	// ALLER A LA PAGE SUCCES
+	
+	page = fs.readFileSync('../html/modele_succes.html', 'utf-8');
+
+	marqueurs = {};
+	marqueurs.erreur = "";
+	page = page.supplant(marqueurs);
+
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write(page);
+	res.end();
+
+};
+
+module.exports = trait;
+
