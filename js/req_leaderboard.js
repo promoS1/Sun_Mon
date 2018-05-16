612ce55817ab792aef22e9bc99e7a883f1c51657
KERRAD <KERRAD@intechS1.fr>
2018-05-16 16:45:38 +0200

    all

---
+++ js/req_leaderboard.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var fs = require("fs");
+require('remedial');
+
+var trait = function (req, res, query) {
+
+	var marqueurs;
+	var pseudo;
+	var mdp;
+	var page;
+
+	//ALLER A LA PAGE DU LEADERBOARD
+
+	page = fs.readFileSync ('../html/modele_leaderboard.html', 'utf-8');
+
+	marqueurs = {};
+	marqueurs.erreurs ="";
+
+	marqueurs.pseudo = query.pseudo;
+	marqueurs.mdp = query.mdp;
+
+	page = page.supplant(marqueurs);
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write(page);
+	res.end();
+
+};
+
+module.exports = trait;
