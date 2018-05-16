cd40529ecfc064d209981680a4b3bd81aeb997ed
RAHAL <RAHAL@intechS1.fr>
2018-05-16 10:28:02 +0200

    req loby solo OK

--- js/req_lobby_solo.js
+++ js/req_lobby_solo.js
@@ -23,7 +23,7 @@ var trait = function (req, res, query) {
 	page = page.supplant(marqueurs);
 	res.writeHead(200, {'Content-Type': 'test/html'});
 	res.write(page);
-	res.end[];
+	res.end();
 
 };
 
