ebfb6fe435138d2788786e982ec34ff0865fbcc5
KERRAD <KERRAD@intechS1.fr>
2018-05-23 15:25:47 +0200

    changement html+js

--- js/req_abandon_solo.js
+++ js/req_abandon_solo.js
@@ -5,20 +5,27 @@ require('remedial');
 
 var trait = function (req, res, query){
 
-
-
 	var marqueurs;
 	var mdp;
-	
+    var pseudo;
+	var page;
+
 	//ALLER A LA PAGE GAGNER
 
-	page = fs.readFileSync('../html/modele_menu');
-
-	marqueurs.abandon="Vous avez abandonnez votre derniere partie";
-
-	//Commun a toutes les pages\\
+	page = fs.readFileSync('../html/modele_menu.html', 'utf-8');
+	
 	marqueurs = {};
-	marqueur.pseudo = query.pseudo;
+
+	marqueurs.pseudo = query.pseudo;
 	marqueurs.mdp = query.mdp;
-	//							\\
-}
+	marqueurs.abandon="Vous avez abandonnez votre derniere partie";
+	page = page.supplant(marqueurs);
+	
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write(page);
+	res.end();
+};
+
+//----------------------------------------------------------------
+
+module.exports = trait;
