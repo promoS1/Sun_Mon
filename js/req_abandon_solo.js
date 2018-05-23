0645597e4edea3692989eed766f7c6f95ec6ced2
KERRAD <KERRAD@intechS1.fr>
2018-05-23 15:35:00 +0200

    changement coup/mw/ml

--- js/req_abandon_solo.js
+++ js/req_abandon_solo.js
@@ -9,18 +9,20 @@ var trait = function (req, res, query){
 	var mdp;
     var pseudo;
 	var page;
-
+	
+	console.log("abandon");
 	//ALLER A LA PAGE GAGNER
 
 	page = fs.readFileSync('../html/modele_menu.html', 'utf-8');
 	
+	//Commun a toutes les pages\\
 	marqueurs = {};
-
 	marqueurs.pseudo = query.pseudo;
 	marqueurs.mdp = query.mdp;
-	marqueurs.abandon="Vous avez abandonnez votre derniere partie";
-	page = page.supplant(marqueurs);
+	marqueurs.abandon="Vous avez abandonné votre derniere partie";
 	
+	//                      \\ 
+	page = page.supplant(marqueurs);
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write(page);
 	res.end();
