ef897eb08ec328224dbeff0dda0186c72f8c5e7b
RAHAL <RAHAL@intechS1.fr>
2018-05-29 16:45:07 +0200

    az

--- js/req_lobby_multi.js
+++ js/req_lobby_multi.js
@@ -6,20 +6,67 @@ require('remedial');
 var trait = function (req, res, query) {
 
 	var marqueurs;
-	var pseudo;
 	var mdp
 	var page;
-
+	var nom;
+	var multi = [];
+	var contenu;
+	var defi;
+	var list= [];
+	var i;
+	var inscrit;
+	
+	
 	//ALLER A LA PAGE LOBBY MULTI
 
 	page = fs.readFileSync ('../html/modele_lobby_multi.html', 'utf-8');
 
 	marqueurs = {};
 	marqueurs.erreurs ="";
-
+	
 	marqueurs.pseudo = query.pseudo;
 	marqueurs.mdp = query.mdp;
+	nom = query.pseudo;
 
+	contenu = fs.readFileSync("json/lobbyMulti.json" , "utf-8")
+	list = JSON.parse(contenu);
+
+
+	inscrit = false;
+	contenu = fs.readFileSync("json/lobbyMulti.json" , "utf-8")
+	multi = JSON.parse(contenu);
+	
+	
+	
+	for (i=0;i<list.length;i++){
+		if (nom === list[i]){
+			inscrit = true;
+			}
+	}	
+
+
+	if (inscrit === false){
+		multi.push(nom);
+		multi = JSON.stringify(multi);
+		fs.writeFileSync("json/lobbyMulti.json", multi , "utf-8");
+	}
+
+
+
+	
+
+
+
+
+
+	defi =" </br>";		
+	for (i=0;i<list.length;i++){
+	defi += "<input type=\"button\" value="+list[i]+" onclick=\"req_demande?"+list[i]+"></br>"
+	}
+
+
+
+	marqueurs.defi = defi
 	page = page.supplant(marqueurs);
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write(page);
