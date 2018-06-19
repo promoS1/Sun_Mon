72b6aab8b1f06b45c382a2e310165faca5d36934
RAHAL <RAHAL@intechS1.fr>
2018-06-19 11:39:13 +0200

    aza

--- js/req_leaderboard.js
+++ js/req_leaderboard.js
@@ -11,9 +11,9 @@ var trait = function (req, res, query) {
 	var page;
 	var contenu;
 	var i ; 			//COMPTEUR BOUCLE FOR
-	var membre = [];
-	var stat = [];
-
+	var membre = [] ;
+	var stat;
+	var list = [];
 	//ALLER A LA PAGE DU LEADERBOARD
 
 	page = fs.readFileSync ('html/modele_leaderboard.html', 'utf-8');
@@ -24,12 +24,31 @@ var trait = function (req, res, query) {
 	marqueurs.pseudo = query.pseudo;
 	marqueurs.mdp = query.mdp;
 	pseudo = query.pseudo;
-	//LIASON DES MEMBRES ET DE LEURS STATS
 
+	//LIASON DES MEMBRES ET DE LEURS STATS
+	contenu = fs.readFileSync("data/membres.json" , "utf-8");
+	membre = JSON.parse(contenu);
+	for(i=0;i<membre.length;i++){
+			try{list[i] = {};
+			list[i].pseudo = membre[i].pseudo;
+			contenu = fs.readFileSync("data/"+membre[i].pseudo+"StatMulti.json" , "utf-8");
+			stat = JSON.parse(contenu);
+			list[i].stat = stat.win;}
+			catch(e){};
+			if (list[i].stat === undefined){
+				list[i].stat = 0;
+			}
+		}
+		
+	console.log(list);
 
 	//ALGORITHME DE TRI
-
-
+	
+	for (i=0;i<list.length;i++){
+		for (y=0;y<list.length;y++){
+			
+		}
+	}
 
 
 	page = page.supplant(marqueurs);
