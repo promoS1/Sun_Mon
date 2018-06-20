57e3f548966bb62ed9fb7866484fb85b9e9aafbd
RAHAL <RAHAL@intechS1.fr>
2018-06-20 10:53:08 +0200

    rt

--- js/req_leaderboard.js
+++ js/req_leaderboard.js
@@ -44,12 +44,45 @@ var trait = function (req, res, query) {
 
 	//ALGORITHME DE TRI
 	
-	for (i=0;i<list.length;i++){
-		for (y=0;y<list.length;y++){
-			
+
+var y;
+var x;
+var change;
+
+for(x=1; x<list.length;x++){
+	y = false;
+	i = x;
+	while(y===false){
+		if (i === 0){
+			i++;
+		}
+		if (list[i].stat > list[(i-1)].stat){
+			change = list[i];
+			list[i] = list[i-1];
+			list[i-1] = change;
+			i--
+		}else {
+			y = true;
 		}
 	}
+}
 
+	//MARQUEURS
+
+	marqueurs.rank1 = list[0].pseudo
+	marqueurs.multiwin1 = list[0].stat
+
+	marqueurs.rank2 = list[1].pseudo
+	marqueurs.multiwin2 = list[1].stat
+
+	marqueurs.rank3 = list[2].pseudo
+	marqueurs.multiwin3 = list[2].stat
+
+	marqueurs.rank4 = list[3].pseudo
+	marqueurs.multiwin4 = list[3].stat
+
+	marqueurs.rank5 = list[4].pseudo
+	marqueurs.multiwin5 = list[4].stat
 
 	page = page.supplant(marqueurs);
 	res.writeHead(200, {'Content-Type': 'text/html'});
